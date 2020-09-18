31b99f912d9c6b61ae41ff9e0ae5890286db7617
mgeaghan <michael.p.geaghan@gmail.com>
2020-09-18 15:35:04 +1000

    new and improved

--- frinky.py
+++ frinky.py
@@ -4,6 +4,21 @@ from PIL import Image
 import numpy as np
 from io import BytesIO
 import math
+import urllib
+import argparse
+import sys
+
+def check_args(args=None):
+	parser = argparse.ArgumentParser(description="Get an ASCII image from frinkiac. Either search for a quote or get a randome one!", formatter_class=argparse.RawTextHelpFormatter)
+	parser.add_argument('-r', '--random', help="Get a random quote", action="store_true")
+	parser.add_argument('-q', '--quote', help="Search for a quote", default="may god have mercy on us all")
+	parser.add_argument('-c', '--caption', help="Custom caption", default="")
+	parser.add_argument('-w', '--width', help="ASCII art width", default="120")
+	parser.add_argument('-g', '--gcf', help="Scaling factor", default="1")
+	arguments = parser.parse_args(args)
+	arguments.width = int(arguments.width)
+	arguments.gcf = int(arguments.gcf)
+	return(arguments)
 
 def get_quote():
 	r = requests.get("https://frinkiac.com/api/random")
@@ -16,6 +31,19 @@ def get_quote():
 		# Combine each line of subtitles into one string.
 		caption = "\n".join([subtitle["Content"] for subtitle in json["Subtitles"]])
 		return image_url, caption
+
+def search_quote(quote_str, episode=None):
+	url = "https://frinkiac.com/api/search?"
+	quote = urllib.parse.urlencode({"q": quote_str})
+	url = url + quote
+	r = requests.get(url)
+	if r.status_code == 200:
+		json = r.json()
+		tophit = json[0]
+		ep = tophit["Episode"]
+		ts = tophit["Timestamp"]
+		image_url = "https://frinkiac.com/meme/" + ep + "/" + str(ts)
+		return image_url
 
 def img2ascii(f, width = 100, GCF = 0.035, url = False):
 	chars = np.asarray(list(' .~^:┐¡Γ░▒╠╬╣▓█'))
@@ -35,11 +63,16 @@ def img2ascii(f, width = 100, GCF = 0.035, url = False):
 	# img = (1*((img - img.min()))/(2*(img.max() - img.min())))**GCF*(chars.size-1)
 	print( "\n".join( ("".join(r) for r in chars[img.astype(int)]) ) )
 
-def f2a(width = 100, GCF = 0.035):
-	furl, quote = get_quote()
-	img2ascii(furl, width = width, GCF = GCF, url = True)
-	print("\n" + quote)
-
 if __name__ == '__main__':
-    f2a(width = 120, GCF = 1)
-
+	cmd_args = check_args(sys.argv[1:])
+	if cmd_args.random:
+		furl, quote = get_quote()
+	else:
+		furl = search_quote(cmd_args.quote)
+		if cmd_args.caption == "":
+			quote = cmd_args.quote.upper()
+		else:
+			quote = cmd_args.caption.upper()
+	img2ascii(furl, width=cmd_args.width, GCF=cmd_args.gcf, url=True)
+	print("\n" + quote)
+	
